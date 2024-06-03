114e9e8b86b847ddee632ffc8995fab1eb538b19
ludihan <65617704+ludihan@users.noreply.github.com>
2024-06-03 16:14:36 -0300

    add reader

--- server/config.go
+++ server/config.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+
 	"github.com/BurntSushi/toml"
 )
 
@@ -15,6 +17,18 @@ func ParseConfigFile(file []byte) (ServerConfig, error) {
     _, err := toml.Decode(string(file), &sc)
     if err != nil {
         return ServerConfig{}, err
+    }
+
+    if sc.AmqpUrl == "" {
+        return ServerConfig{}, errors.New("Empty url")
+    }
+
+    if sc.Port == "" {
+        return ServerConfig{}, errors.New("Empty port")
+    }
+
+    if len(sc.FilterPattern) == 0 {
+        return ServerConfig{}, errors.New("Empty filters")
     }
 
     return sc, nil
