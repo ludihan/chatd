c9093bea7e419a6046bcf9584b48974fbfc14d6c
ludihan <65617704+ludihan@users.noreply.github.com>
2024-06-05 00:25:53 -0300

    format tui-client

--- tui-client/chat.go
+++ tui-client/chat.go
@@ -29,95 +29,95 @@ type (
 )
 
 type model struct {
-	viewport    viewport.Model
-	messages    []string
-	textarea    textarea.Model
-	senderStyle lipgloss.Style
-	err         error
-    conn *amqp.Connection
-    ch *amqp.Channel
-    exchange string
-    nome string
-    urlAmqp string
-    urlApi string
-    msgs <-chan amqp.Delivery
-    previousMessage string
+	viewport        viewport.Model
+	messages        []string
+	textarea        textarea.Model
+	senderStyle     lipgloss.Style
+	err             error
+	conn            *amqp.Connection
+	ch              *amqp.Channel
+	exchange        string
+	nome            string
+	urlAmqp         string
+	urlApi          string
+	msgs            <-chan amqp.Delivery
+	previousMessage string
 }
 
 func initialModel() model {
-    if len(os.Args) < 5 {
-        fmt.Println("Informe o seu nome, a exchange, o urlAmqp do amqp e o url da api")
-        os.Exit(1)
-    }
+	if len(os.Args) < 5 {
+		fmt.Println("Informe o seu nome, a exchange, o urlAmqp do amqp e o url da api")
+		os.Exit(1)
+	}
 
-    nome := os.Args[1]
-    exchange := os.Args[2]
-    urlAmqp := os.Args[3]
-    urlApi := os.Args[4]
+	nome := os.Args[1]
+	exchange := os.Args[2]
+	urlAmqp := os.Args[3]
+	urlApi := os.Args[4]
 
 	conn, err := amqp.Dial(urlAmqp)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	ch, err := conn.Channel()
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    err = ch.ExchangeDeclare(
-        exchange, // name
-        "fanout",                // type
-        false,                   // durable
-        true,                    // auto-deleted
-        false,                   // internal
-        false,                   // no-wait
-        nil,                     // arguments
-    )
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = ch.ExchangeDeclare(
+		exchange, // name
+		"fanout", // type
+		false,    // durable
+		true,     // auto-deleted
+		false,    // internal
+		false,    // no-wait
+		nil,      // arguments
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    q, err := ch.QueueDeclare(
-        "",    // name
-        false, // durable
-        true, // delete when unused
-        true,  // exclusive
-        false, // no-wait
-        nil,   // arguments
-    )
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		true,  // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    err = ch.QueueBind(
-        q.Name, // queue name
-        "",     // routing key
-        exchange, // exchange
-        false,
-        nil,
-    )
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	err = ch.QueueBind(
+		q.Name,   // queue name
+		"",       // routing key
+		exchange, // exchange
+		false,
+		nil,
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    msgs, err := ch.Consume(
-        q.Name, // queue
-        "",     // consumer
-        true,   // auto-ack
-        false,  // exclusive
-        false,  // no-local
-        false,  // no-wait
-        nil,    // args
-    )
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	ta := textarea.New()
 	ta.Placeholder = "Mande uma mensagem"
@@ -145,13 +145,13 @@ func initialModel() model {
 		viewport:    vp,
 		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
 		err:         nil,
-        conn: conn,
-        ch: ch,
-        exchange: exchange,
-        nome: nome,
-        urlAmqp: urlAmqp,
-        urlApi: urlApi,
-        msgs: msgs,
+		conn:        conn,
+		ch:          ch,
+		exchange:    exchange,
+		nome:        nome,
+		urlAmqp:     urlAmqp,
+		urlApi:      urlApi,
+		msgs:        msgs,
 	}
 }
 
@@ -168,57 +168,56 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	m.textarea, tiCmd = m.textarea.Update(msg)
 	m.viewport, vpCmd = m.viewport.Update(msg)
 
-    select {
-    case newMessage := <- m.msgs:
-        message := struct {
+	select {
+	case newMessage := <-m.msgs:
+		message := struct {
 			Exchange string `json:"exchange"`
 			Body     string `json:"body"`
 			UserId   string `json:"userId"`
-        }{}
-    json.Unmarshal(newMessage.Body, &message)
-    if message.Body != m.previousMessage {
-        m.messages = append(m.messages, m.senderStyle.Render(fmt.Sprintf("%v: ", message.UserId))+message.Body)
-        m.viewport.SetContent(strings.Join(m.messages, "\n"))
-        m.textarea.Reset()
-        m.viewport.GotoBottom()
-    }
+		}{}
+		json.Unmarshal(newMessage.Body, &message)
+		if message.Body != m.previousMessage {
+			m.messages = append(m.messages, m.senderStyle.Render(fmt.Sprintf("%v: ", message.UserId))+message.Body)
+			m.viewport.SetContent(strings.Join(m.messages, "\n"))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
+		}
 
-    default:
-        switch msg := msg.(type) {
-        case tea.KeyMsg:
-            switch msg.Type {
-            case tea.KeyCtrlC, tea.KeyEsc:
-                fmt.Println(m.textarea.Value())
-                m.ch.Close()
-                m.conn.Close()
-                return m, tea.Quit
-            case tea.KeyEnter:
-                m.messages = append(m.messages, m.senderStyle.Render(fmt.Sprintf("%v: ", m.nome))+m.textarea.Value())
-                m.previousMessage = m.textarea.Value()
+	default:
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.Type {
+			case tea.KeyCtrlC, tea.KeyEsc:
+				fmt.Println(m.textarea.Value())
+				m.ch.Close()
+				m.conn.Close()
+				return m, tea.Quit
+			case tea.KeyEnter:
+				m.messages = append(m.messages, m.senderStyle.Render(fmt.Sprintf("%v: ", m.nome))+m.textarea.Value())
+				m.previousMessage = m.textarea.Value()
 
-                body, _ := json.Marshal(map[string]string{
-                    "exchange": m.exchange,
-                    "body": m.textarea.Value(),
-                    "userId": m.nome,
-                })
+				body, _ := json.Marshal(map[string]string{
+					"exchange": m.exchange,
+					"body":     m.textarea.Value(),
+					"userId":   m.nome,
+				})
 
-                payload := bytes.NewBuffer(body)
+				payload := bytes.NewBuffer(body)
 
-                http.Post(m.urlApi, "application/json", payload)
+				http.Post(m.urlApi, "application/json", payload)
 
+				m.viewport.SetContent(strings.Join(m.messages, "\n"))
+				m.textarea.Reset()
+				m.viewport.GotoBottom()
+			}
 
-                m.viewport.SetContent(strings.Join(m.messages, "\n"))
-                m.textarea.Reset()
-                m.viewport.GotoBottom()
-            }
-
-            // We handle errors just like any other message
-        case errMsg:
-            m.err = msg
-            return m, nil
-        }
-    }
-    return m, tea.Batch(tiCmd, vpCmd)
+			// We handle errors just like any other message
+		case errMsg:
+			m.err = msg
+			return m, nil
+		}
+	}
+	return m, tea.Batch(tiCmd, vpCmd)
 
 }
 
